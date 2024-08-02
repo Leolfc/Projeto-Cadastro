ba0bb1d637a7cd86af73d66602ff23fa31605ce3
Leolfc <147149946+Leolfc@users.noreply.github.com>
2024-08-01 21:39:01 -0300

    Projeto Cadastro

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
+import Cadastro from "./Cadastro";
+import Login from "./Login";
+
+
+function App() {
+
+
+  return (
+    <BrowserRouter>
+    <Routes>
+    <Route path="/"element={<Cadastro/>}/>
+    <Route path="/"element={<Login />}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/Cadastro/index.jsx
@@ -0,0 +1,30 @@
+import { useRef } from "react";
+import { Link } from "react-router-dom";
+
+function Cadastro() {
+    const nameRef = useRef()
+    const email = useRef()
+    const passwordRef = useRef()
+
+function handleSubmit(event){
+    event.preventDefault()
+    console.log(nameRef)
+}
+
+
+  return (
+    <div className="max-w-md mx-auto mt-10 bg-white p-8  border-gray-300 rounded-lg shadow-lg">
+      <h2 className="text-2xl font-bold mb-6 text-center text-gray-800 ">Cadastro</h2>
+      <form action="" className="flex flex-col gap-6 " onSubmit={handleSubmit}>
+        <input ref={nameRef} placeholder="Nome" type="text" name="" id="" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none" />
+        <input ref={email} placeholder="Email" type="email" name="" id="" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none" />
+        <input ref={passwordRef} placeholder="Senha" type="password" name="" id="" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none" />
+
+        <button className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-400">Cadastrar-se</button>
+      </form>
+      <Link className="m-4 block text-center" to={'/Login'}>Ja tem uma conta? Faça login</Link>
+    </div>
+  );
+}
+
+export default Cadastro;
